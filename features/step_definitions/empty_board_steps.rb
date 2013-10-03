57fa30e5532c82cd50de3a3437d830e2fb530c3b
socialchorus <socialchorus@socialchoruss-MacBook-Pro-10.local>
2013-10-03 16:00:25 -0700

    WIP cukes

--- features/step_definitions/empty_board_steps.rb
+++ features/step_definitions/empty_board_steps.rb
@@ -1,15 +1,18 @@
 Given(/^I have started the game$/) do
-	@output = `ruby sudoku.rb` 
+  @output = `ruby sudoku.rb` 
+  puts @output.inspect
 end
 
 Then(/^I will see an empty board$/) do
-  @output.should_not match /\d/	
-  @output.should match /\D/ # express the regexp above with the code you wish you had
+  @output.should_not match /\d/ 
+  @output.should match /\|/
+# That regular expression should exactly match our board. 
+#It checks that it generated correctly and checks that it's a 4x4.
 end
 
-	# we create a board class and the initialize method creates a 
-	# 4x4 board that is empty
-	# there are 16 objects... 
+  # we create a board class and the initialize method creates a 
+  # 4x4 board that is empty
+  # there are 16 objects... 
 
 #assert some output
 #no numbers
